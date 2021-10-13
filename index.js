1bc25eacb2d77875d75b0bf1fdfd9448e83a9f8d
Saurabh Burewar <burewar.1@iitj.ac.in>
2021-10-13 23:36:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,136 @@
+groups = [];
+username = 'saurabhburewar';
+
+function display(data) {
+    for(let i = 0; i < data.length; i++) {
+        let name = data[i].name;
+        group = name.substring(0, name.indexOf('_'));
+        if (group == '') {
+            group = 'Others';
+        }
+        repo = name.substring(name.indexOf('_')+1, name.length);
+        if (groups.includes(group)) {
+            item = document.createElement('a');
+            item.className = 'groupItem';
+            item.href = data[i].html_url;
+            item.innerHTML = repo;
+            let index = groups.indexOf(group);
+            document.getElementById(`group${index}Box`).appendChild(item)
+        } else {
+            groups.push(group);
+            let ind = groups.indexOf(group);
+
+            groupdiv = document.createElement('div');
+            groupdiv.className = 'groupBox';
+            groupdiv.id = `group${ind}Box`;
+
+            grouphead = document.createElement('div');
+            grouphead.className = 'groupHead';
+            grouphead.innerHTML = group;
+            groupdiv.appendChild(grouphead);
+
+            item = document.createElement('a');
+            item.className = 'groupItem';
+            item.href = data[i].html_url;
+            item.innerHTML = repo;
+            groupdiv.appendChild(item);
+
+            document.getElementById('listboxin').appendChild(groupdiv);
+        }
+    }
+}
+
+function displaycontri(data1) {
+    for(let i = 0; i < data1.length; i++) {
+        if (groups.includes('Contributor')) {
+            contriItem = document.createElement('a');
+            contriItem.className = 'contriItem';
+            contriItem.href = data1[i].html_url
+            contriItem.innerHTML = data1[i].name;
+            document.getElementById('contriBox').appendChild(contriItem);
+        } else {
+            groups.push('Contributor');
+
+            contriBox = document.createElement('div');
+            contriBox.id = 'contriBox';
+
+            contrihead = document.createElement('div');
+            contrihead.id = 'contriHead';
+            contrihead.innerHTML = 'Contributor';
+            contriBox.appendChild(contrihead);
+
+            contriItem = document.createElement('a');
+            contriItem.className = 'contriItem';
+            contriItem.href = data1[i].html_url
+            contriItem.innerHTML = data1[i].name;
+            contriBox.appendChild(contriItem);
+
+            document.getElementById('listboxin').appendChild(contriBox);
+        }
+    }
+}
+
+function changePage(userdata) {
+    document.title = `${userdata.login}/repos`
+    document.getElementById('bannerName1').innerHTML = userdata.name;
+    document.getElementById('bannerName2').innerHTML = userdata.login;
+    document.getElementById('bannerPic').style.backgroundImage = `url(${userdata.avatar_url})`;
+    link = document.createElement('link');
+    link.rel = 'icon';
+    link.href = userdata.avatar_url;
+    document.getElementsByTagName('head')[0].appendChild(link);
+
+    if (userdata.company != null) {
+        comdiv = document.createElement('a');
+        comdiv.className = 'bannerlinks';
+        comdiv.id = 'comlink';
+        comdiv.href = userdata.company;
+        comdiv.innerHTML = userdata.company;
+        document.getElementById('bannerlinkBox1').appendChild(comdiv);
+    }
+    if (userdata.location != null) {
+        locdiv = document.createElement('a');
+        locdiv.className = 'bannerlinks';
+        locdiv.id = 'loclink';
+        locdiv.href = userdata.location;
+        locdiv.innerHTML = userdata.location;
+        document.getElementById('bannerlinkBox1').appendChild(locdiv);
+    }
+    if (userdata.blog != null) {
+        blogdiv = document.createElement('a');
+        blogdiv.className = 'bannerlinks';
+        blogdiv.id = 'bloglink';
+        blogdiv.href = userdata.blog;
+        blogdiv.innerHTML = userdata.blog;
+        document.getElementById('bannerlinkBox1').appendChild(blogdiv);
+    }
+    if (userdata.twitter_username != null) {
+        tdiv = document.createElement('a');
+        tdiv.className = 'bannerlinks';
+        tdiv.id = 'tweetlink';
+        tdiv.href = userdata.twitter_username;
+        tdiv.innerHTML = userdata.twitter_username;
+        document.getElementById('bannerlinkBox1').appendChild(tdiv);
+    }
+    if (userdata.email != null) {
+        ediv = document.createElement('a');
+        ediv.className = 'bannerlinks';
+        ediv.id = 'emaillink';
+        ediv.href = userdata.email;
+        ediv.innerHTML = `Email: ${userdata.email}`;
+        document.getElementById('bannerlinkBox1').appendChild(ediv);
+    }
+}
+
+
+fetch(`https://api.github.com/users/${username}`)
+    .then(response => response.json())
+    .then(userdata => changePage(userdata))
+
+fetch(`https://api.github.com/users/${username}/repos?type=owner`)
+    .then(response => response.json())
+    .then(data => display(data))
+
+fetch(`https://api.github.com/users/${username}/repos?type=member`)
+    .then(res => res.json())
+    .then(data1 => displaycontri(data1))
