076994f5adcd12908d3930794be33c2278d548b7
Saurabh Burewar <burewar.1@iitj.ac.in>
2023-02-11 00:54:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 groups = [];
 username = 'saurabhburewar';
 
-const tl = gsap.timeline({ defaults: { ease: "power1.out" }, repeat: -1, repeatDelay: 1});
+// const tl = gsap.timeline({ defaults: { ease: "power1.out" }, repeat: -1, repeatDelay: 1});
 
-tl.to(".bannerneonbar2", { y: '500%', duration: 2, stagger: 0.25 });
-tl.to(".bannerneonbar2", { opacity: 0, duration: 1, stagger: 0.25, delay: -1 });
-tl.to('.bannerneonbar3', { x: "-400%", opacity: 1, duration: 2, stagger: 0.25, delay: -1 });
-tl.to(".bannerneonbar3", { opacity: 0, duration: 1, stagger: 0.25 });
-tl.to(".bannerneonbar1", { y: '500%', duration: 3, stagger: 0.25, delay: -4 });
-tl.to(".bannerneonbar1", { opacity: 0, duration: 1, stagger: 0.25, delay: -1 });
+// tl.to(".bannerneonbar2", { y: '500%', duration: 2, stagger: 0.25 });
+// tl.to(".bannerneonbar2", { opacity: 0, duration: 1, stagger: 0.25, delay: -1 });
+// tl.to('.bannerneonbar3', { x: "-400%", opacity: 1, duration: 2, stagger: 0.25, delay: -1 });
+// tl.to(".bannerneonbar3", { opacity: 0, duration: 1, stagger: 0.25 });
+// tl.to(".bannerneonbar1", { y: '500%', duration: 3, stagger: 0.25, delay: -4 });
+// tl.to(".bannerneonbar1", { opacity: 0, duration: 1, stagger: 0.25, delay: -1 });
 
 function display(data) {
     for(let i = 0; i < data.length; i++) {
@@ -88,38 +88,17 @@ function changePage(userdata) {
     link.rel = 'icon';
     link.href = userdata.avatar_url;
     document.getElementsByTagName('head')[0].appendChild(link);
+    console.log(userdata);
 
-    if (userdata.company != null) {
-        comdiv = document.createElement('a');
-        comdiv.className = 'bannerlinks';
-        comdiv.id = 'comlink';
-        comdiv.href = userdata.company;
-        comdiv.innerHTML = userdata.company;
-        document.getElementById('bannerlinkBox1').appendChild(comdiv);
-    }
-    if (userdata.location != null) {
-        locdiv = document.createElement('a');
-        locdiv.className = 'bannerlinks';
-        locdiv.id = 'loclink';
-        locdiv.href = userdata.location;
-        locdiv.innerHTML = userdata.location;
-        document.getElementById('bannerlinkBox1').appendChild(locdiv);
-    }
     if (userdata.blog != null) {
         blogdiv = document.createElement('a');
         blogdiv.className = 'bannerlinks';
         blogdiv.id = 'bloglink';
         blogdiv.href = userdata.blog;
         blogdiv.innerHTML = userdata.blog;
+        blogdiv.style.display = 'flex'
+        blogdiv.style.justifyContent = 'center'
         document.getElementById('bannerlinkBox1').appendChild(blogdiv);
-    }
-    if (userdata.twitter_username != null) {
-        tdiv = document.createElement('a');
-        tdiv.className = 'bannerlinks';
-        tdiv.id = 'tweetlink';
-        tdiv.href = userdata.twitter_username;
-        tdiv.innerHTML = userdata.twitter_username;
-        document.getElementById('bannerlinkBox1').appendChild(tdiv);
     }
     if (userdata.email != null) {
         ediv = document.createElement('a');
@@ -127,7 +106,37 @@ function changePage(userdata) {
         ediv.id = 'emaillink';
         ediv.href = userdata.email;
         ediv.innerHTML = `Email: ${userdata.email}`;
+        ediv.style.display = 'flex'
+        ediv.style.justifyContent = 'center'
         document.getElementById('bannerlinkBox1').appendChild(ediv);
+    }
+    if (userdata.twitter_username != null) {
+        tdiv = document.createElement('a');
+        tdiv.className = 'bannerlinks';
+        tdiv.id = 'tweetlink';
+        tdiv.href = userdata.twitter_username;
+        tdiv.innerHTML = userdata.twitter_username;
+        tdiv.style.display = 'flex'
+        tdiv.style.justifyContent = 'center'
+        document.getElementById('bannerlinkBox1').appendChild(tdiv);
+    }
+    if (userdata.company != null) {
+        comdiv = document.createElement('div');
+        comdiv.className = 'bannerlinks';
+        comdiv.id = 'comlink';
+        comdiv.innerHTML = userdata.company;
+        comdiv.style.display = 'flex'
+        comdiv.style.justifyContent = 'center'
+        document.getElementById('bannerlinkBox1').appendChild(comdiv);
+    }
+    if (userdata.location != null) {
+        locdiv = document.createElement('div');
+        locdiv.className = 'bannerlinks';
+        locdiv.id = 'loclink';
+        locdiv.innerHTML = userdata.location;
+        locdiv.style.display = 'flex'
+        locdiv.style.justifyContent = 'center'
+        document.getElementById('bannerlinkBox1').appendChild(locdiv);
     }
 }
 
@@ -139,7 +148,6 @@ fetch(`https://api.github.com/users/${username}`)
             .then(response => response.json())
             .then(data => {
                 display(data);
-                console.log(data)
                 fetch(`https://api.github.com/users/${username}/repos?per_page=100&type=member`)
                     .then(res => res.json())
                     .then(data1 => displaycontri(data1))
